976e790a8095f58c39e747151aa786e15362faf6
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-21 08:34:21 -0300

    add class 01 primitive types

---
+++ Python/class-01-primitive_types.py
@@ -0,0 +1,21 @@
+name = "Emanuel" #string 
+age = 30 #int
+height = 1.85 #float
+is_admin = True #boolean
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_admin))
+
+print("Sou " + name + " e tenho " + str(age) + " anos")
+# print(f'Sou {name} tenho {float(age)} anos e {height} de altura')
+
+# new_age = float(age)
+# new_height = int(height)
+
+# print(new_age)
+# print(type(new_age))
+
+# print(new_height)
+# print(type(new_height))
