34ebc9a62e660f929d884b20be70cad05d98c177
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-22 16:55:55 -0300

    add data types example dict

--- Python/class-01-data_types.py
+++ Python/class-01-data_types.py
@@ -31,26 +31,45 @@ is_admin = True #boolean
 
 # Collections 
 
-array = ["string", 2, True, [2, "Três"]]
+list = ["string", 2, True, [2, "Três"]]
 #           0      1    2         3
 
-# print(array[0])
-# print(array[-2])
-# print(array[3][1])
+# print(list[0])
+# print(list[-2])
+# print(list[3][1])
 
-array[2] = False
+list[2] = False
 
-# print(array)
-# print(len(array))
+# print(list)
+# print(len(list))
+
 
 tuple = ("string", 2, True, [2, "Três"])
-tuple = ("cpf", "123.123.123-12", 12312312312)
+# tuple = ("cpf", "123.123.123-12", 12312312312)
 
-print(tuple[0])
-print(tuple[-2])
-print(tuple[3][1])
+# print(tuple[0])
+# print(tuple[-2])
+# print(tuple[3][1])
 
-tuple[2] = False
+# tuple[2] = False
 
-print(tuple)
-print(len(tuple))
+# print(tuple)
+# print(len(tuple))
+
+
+set = {"Violão", "Guitarra", "Bateria"}
+set.add("Baixo")
+# set.remove("Violão")
+set.pop()
+# print(set)
+
+
+dict = {
+  "name":"Emanuel",
+  "age":30,
+  "is_admin": False,
+  "favorites": ["Matrix", "Jurassic Park", "The Terminator"]
+}
+
+# print(dict["favorites"][1])
+print(dict.get("favorites", "Não foi possível recuperar dado")[1])
