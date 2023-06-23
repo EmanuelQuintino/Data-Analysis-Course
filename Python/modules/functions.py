7720ab457fdbc8c7824808d40cf96a99da12b00b
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-23 08:11:40 -0300

    update spaces

--- Python/modules/functions.py
+++ Python/modules/functions.py
@@ -2,13 +2,16 @@ def sum(num1, num2):
   total = num1 + num2
   return total
 
+
 def sub(num1, num2):
   total = num1 - num2
   return total
 
+
 def mul(num1, num2):
   total = num1 * num2
   return total
+
 
 def div(num1, num2):
   total = num1 / num2
