bfe9ca2dfa7aa386f1b25e82924b707453c125da
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-23 08:45:08 -0300

    add folder files

--- Python/class-07-file_manipulation.py
+++ Python/class-07-file_manipulation.py
@@ -2,38 +2,39 @@
 
 import os
 
-file = open("Python/class-07-list.txt", "r+", encoding='utf-8')
-file2 = open("Python/class-07-list2.txt", "w", encoding='utf-8')
+file = open("Python/files/object_list.txt", "r", encoding='utf-8')
+file2 = open("Python/files/object_list2.txt", "w", encoding='utf-8')
 
 # "r" - read
-# print(file.read())
+print(file.read())
 # print(file.readline())
 # print(file.readline())
 # print(file.readline())
 
+file.seek(0)
 list = file.readlines()
 print(list)
 
-for i in list:
-  print(i.upper())
-  file2.write(f"{i.upper()}")
+# for i in list:
+#   print(i.upper())
+#   file2.write(f"{i.upper()}")
 
-# "a" - append
-# "w" - write
-# file.write("avião\n")
-# file2.write("avião\n")
+# # "a" - append
+# # "w" - write
+# # file.write("avião\n")
+# # file2.write("avião\n")
 
-# file2 = open("Python/class-07-list.txt", "w", encoding='utf-8')
-item = "bola"
-new_list = []
-for i in list:
-  if i.strip() != item:
-    new_list.append(i)
-    file2.write(i)
+# # file2 = open("Python/files/object_list.txt", "w", encoding='utf-8')
+# item = "bola"
+# new_list = []
+# for i in list:
+#   if i.strip() != item:
+#     new_list.append(i)
+#     file2.write(i)
 
-print(new_list)
+# print(new_list)
 
 file.close()
 file2.close()
 
-# os.remove("Python/class-07-list2.txt")
+os.remove("Python/files/object_list2.txt")
