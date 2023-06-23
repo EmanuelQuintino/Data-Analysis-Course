30c270881bcf9cc4186eb6559da337f45efa3791
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-23 08:10:53 -0300

    add class modularization

---
+++ Python/class-08-modularization.py
@@ -0,0 +1,12 @@
+from modules import constants
+from modules import functions
+import math
+
+result_sum = functions.sum(2, 3)
+result_mul = functions.mul(2, 3)
+print(result_mul, constants.GRAVITY)
+
+print(math.sqrt(9))
+print(math.floor(5.5))
+print(math.ceil(5.5))
+print(math.pow(2, 10))
---
+++ Python/modules/functions.py
@@ -0,0 +1,15 @@
+def sum(num1, num2):
+  total = num1 + num2
+  return total
+
+def sub(num1, num2):
+  total = num1 - num2
+  return total
+
+def mul(num1, num2):
+  total = num1 * num2
+  return total
+
+def div(num1, num2):
+  total = num1 / num2
+  return total
