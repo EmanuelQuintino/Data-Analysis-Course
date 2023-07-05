30e99debdaf8f60134c2e9c78c73059aa453c292
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-05 16:31:00 -0300

    add calculator operation validation

--- 3-Python/challenges/calculator.py
+++ 3-Python/challenges/calculator.py
@@ -10,15 +10,17 @@ if operation == "+":
   result = num1 + num2
   print(f"A soma de {num1} e {num2} é {result}")
 
-if operation == "-":
+elif operation == "-":
   result = num1 - num2
   print(f"A subtração de {num1} e {num2} é {result}")
 
-if operation == "*":
+elif operation == "*":
   result = num1 * num2
   print(f"A multiplicação de {num1} e {num2} é {result}")
 
-if operation == "/":
+elif operation == "/":
   result = num1 / num2
   print(f"A divisão de {num1} e {num2} é {result:.2f}")
-  
+
+else:
+  print("Operação inválida!")
