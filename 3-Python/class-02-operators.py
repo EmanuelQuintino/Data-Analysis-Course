4b2f00def8a4a039ab13a9b44779b9f948268650
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-10 10:42:53 -0300

    add comparison and logical operators

--- 3-Python/class-02-operators.py
+++ 3-Python/class-02-operators.py
@@ -1,29 +1,55 @@
 # Arithmetic
+
 num1 = 3
 num2 = 2
 num3 = -9.4
 
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
-print(num1 ** num2)
-print(num1 % num2)
-print(num1 // num2)
-print(num3 ** (1/2))
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 ** num2)
+# print(num1 % num2)
+# print(num1 // num2)
+# print(num3 ** (1/2))
 
-print((num1 + num2) * num3)
+# print((num1 + num2) * num3)
 
-print(pow(num1, num2))
-print(abs(num3))
-print(round(num3))
+# print(pow(num1, num2))
+# print(abs(num3))
+# print(round(num3))
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+# num1 = float(input("Digite o primeiro número: "))
+# num2 = float(input("Digite o segundo número: "))
 sum = num1 + num2
 
-print(f"A soma de {num1} e {num2} é {sum}")
+# print(f"A soma de {num1} e {num2} é {sum}")
 
-# Assignment
 # Comparison
+
+# print(5 == 5)
+# print(3 != 2)
+# print(4 != 4)
+# print(6 > 3)
+# print(2 < 1)
+# print(10 >= 10)
+# print(6 <= 3)
+
 # Logical
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+# print(not True)
+
+print(2 == 2 and 3 == 3)
+print(2 != 2 and 3 == 3)
+print(2 != 2 or 3 == 3)
+print(2 != 2 or not 3 == 3)
