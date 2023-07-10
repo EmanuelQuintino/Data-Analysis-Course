50461fd1d50ec3b78a35a1fc06100c09be4c6eb8
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-10 11:48:34 -0300

    update class loop

--- 3-Python/class-06-loops.py
+++ 3-Python/class-06-loops.py
@@ -1,23 +1,65 @@
 # Loops
 
+array_numbers = [1, 2, 3, 4, 5]
+
 # i = 1
-# while i <= 10:
+# while i <= 5:
 #   print(i)
 #   i += 1
 
-for i in range(5, 11, 2):
-  print(i)
+# i = 0
+# while i < 5:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)
+
+# i = 1
+# while i <= len(array_numbers):
+#   print(array_numbers[i - 1])
+#   i += 1
+
+# for i in range(1, 6, 2):
+#   print(i)
+
+# for i in array_numbers:
+#   print(i)
+
+# name = "Emanuel"
+# for i in name:
+#   print(i)
 
 # list_names = ["Pedro", "João", "Carla", "Clara"]
 # for name in list_names:
 #   print(name)
 
-array = [
-  ["A", "B", "C"],
-  ["D", "E", "F"],
-  ["G", "H", "I"],
+# array = [
+#   ["A", "B", "C"],
+#   ["D", "E", "F"],
+#   ["G", "H", "I"],
+# ]
+
+# for i in array:
+#   for j in i:
+#     print(j)
+
+list_users = [
+  {"name":"Carla", "age": 14},
+  {"name":"Pedro", "age": 22},
+  {"name":"João", "age": 25},
+  {"name":"Clara", "age": 18},
 ]
 
-for i in array:
-  for j in i:
-    print(j)
+# for user in list_users:
+#   if user["age"] > 20:
+#     print(f"{user['name']} tem {user['age']} anos")
+
+# for user in list_users:
+#   if user["name"] == "Carla":
+#     print(f"{user['name']} tem {user['age']} anos")
+#     break
+#   print("-")
+
+# for user in list_users:
+#   for k, v in user.items():
+#     print(v)
