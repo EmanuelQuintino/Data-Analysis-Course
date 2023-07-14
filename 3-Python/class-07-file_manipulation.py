eb66030597fa4a5a7f73e2c6d68d3157f691f080
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-14 19:49:09 -0300

    add example pandas

--- 3-Python/class-07-file_manipulation.py
+++ 3-Python/class-07-file_manipulation.py
@@ -1,30 +1,31 @@
 # File Manipulation
+# "r" - read
+# "w" - write
+# "a" - append
 
 import os
 
-file = open("Python/files/object_list.txt", "r", encoding='utf-8')
-file2 = open("Python/files/object_list2.txt", "w", encoding='utf-8')
+# file = open("3-Python/files/object_list.txt", "r", encoding='utf-8')
+# file2 = open("3-Python/files/object_list2.txt", "w", encoding='utf-8')
+# file2 = open("3-Python/files/object_list2.txt", "a", encoding='utf-8')
 
-# "r" - read
-print(file.read())
-# print(file.readline())
-# print(file.readline())
+# print(file.read())
+# print(file.tell())
 # print(file.readline())
 
-file.seek(0)
-list = file.readlines()
-print(list)
+# file.seek(0)
+# list = file.readlines()
+# print(list)
 
 # for i in list:
 #   print(i.upper())
 #   file2.write(f"{i.upper()}")
 
-# # "a" - append
-# # "w" - write
-# # file.write("avião\n")
-# # file2.write("avião\n")
+# file.write("avião\n")
+# file2.write("avião\n")
+# file2.write("bola\n")
 
-# # file2 = open("Python/files/object_list.txt", "w", encoding='utf-8')
+# # file2 = open("3-Python/files/object_list.txt", "w", encoding='utf-8')
 # item = "bola"
 # new_list = []
 # for i in list:
@@ -34,7 +35,37 @@ print(list)
 
 # print(new_list)
 
-file.close()
-file2.close()
+# file.close()
+# file2.close()
 
-os.remove("Python/files/object_list2.txt")
+# os.remove("Python/files/object_list2.txt")
+
+# Exercise
+# employees = open("3-Python/files/employee_salaries.csv", "r", encoding='utf-8')
+
+# dataset = employees.read()
+
+# print(dataset)
+
+# rows = dataset.split("\n")
+# print(rows)
+
+# arrayEmployees = []
+
+# for row in rows:
+  # split_row = row.split(",")
+  # arrayEmployees.append(split_row)
+
+# print(arrayEmployees)
+# print(len(arrayEmployees))
+# print(len(arrayEmployees[0]))
+
+# employees.close()
+
+import pandas as pd
+
+employees = "3-Python/files/employee_salaries.csv"
+dataset = pd.read_csv(employees)
+
+print(dataset)
+print(dataset.info())
