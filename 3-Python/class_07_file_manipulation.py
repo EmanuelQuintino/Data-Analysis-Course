ab0bf671f1b3508285139bfb72a9cfc600f36347
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-16 19:18:25 -0300

    add example csv

--- 3-Python/class_07_file_manipulation.py
+++ 3-Python/class_07_file_manipulation.py
@@ -40,7 +40,7 @@ with open("3-Python/files/object_list.txt", "r", encoding='utf-8') as file:
 # file.close()
 # file2.close()
 
-import os
+# import os
 # os.remove("3-Python/files/object_list2.txt")
 
 # Exercise
@@ -64,6 +64,33 @@ import os
 # print(len(arrayEmployees[0]))
 
 # employees.close()
+
+# File CSV
+
+import csv
+
+dados = [
+    ['Name', 'Age', 'City'],
+    ['João', 25, 'São Paulo'],
+    ['Maria', 30, 'Rio de Janeiro'],
+    ['Pedro', 35, 'Belo Horizonte'],
+    ['Ana', 28, 'Brasília']
+]
+
+file = '3-Python/files/studants.csv'
+with open(file, 'w', newline='\n', encoding='utf-8') as file_csv:
+  writer = csv.writer(file_csv)
+  for linha in dados:
+    writer.writerow(linha)
+
+with open("3-Python/files/studants.csv", "r", encoding='utf-8', newline="\n") as file_csv:
+  reader = csv.reader(file_csv)
+  data = list(reader)
+  print(data)
+
+  for i in data[1:]:
+    print(i)
+
 
 # import pandas as pd
 
