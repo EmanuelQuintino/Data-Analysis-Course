1db28ccd997b59274ed530401b110313eac5c993
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-30 16:24:14 -0300

    add class oop example attributes and methods

---
+++ 3-Python/class_10_oop/attributes_methods.py
@@ -0,0 +1,12 @@
+class Student:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def matriculate(self):
+    print(f"{self.name} matriculado(a) com sucesso!")
+
+student = Student("Emanuel", "30")
+print(student.name)
+print(student.age)
+student.matriculate()
