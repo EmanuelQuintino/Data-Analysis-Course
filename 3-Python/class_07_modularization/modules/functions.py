39f310b59beca62261732195576a00aef8afaf7f
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-01-14 15:40:27 -0300

    remove pycache

--- 3-Python/class_07_modularization/modules/functions.py
+++ 3-Python/class_07_modularization/modules/functions.py
@@ -2,16 +2,13 @@ def sum(num1, num2):
   total = num1 + num2
   return total
 
-
 def sub(num1, num2):
   total = num1 - num2
   return total
 
-
 def mul(num1, num2):
   total = num1 * num2
   return total
-
 
 def div(num1, num2):
   total = num1 / num2
