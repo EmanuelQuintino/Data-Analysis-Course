203a65b6a63c639db173978d70215132ed696abc
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-04-30 18:29:51 -0300

    update class 01 primitive types examples

--- 4-Python/class_01_data_types/main.py
+++ 4-Python/class_01_data_types/main.py
@@ -1,87 +1,87 @@
-# Variables
+# variables
 
-name1 = "Test1"
-name2 = "Test2"
+name1 = "test1"
+name2 = "test2"
 name2 = name1
 
-name1, name2 = "Test1", "test2"
-name1, name2 = "Test1", "test2"
+name1, name2 = "test1", "test2"
+name1, name2 = "test1", "test2"
 
-name1 = name2 = "Test1"
+name1 = name2 = "test1"
 
 print(name1 * 3)
 print(name2[::-1])
 
-# Primitive Types
+# primitive types
 
 name = "Emanuel" #string 
 age = 30 #int
 height = 1.85 #float
 is_admin = True #boolean
 
-# print(type(name))
-# print(type(age))
-# print(type(height))
-# print(type(is_admin))
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_admin))
 
-# name = "Jorge" 
-# age = 20
+name = "Jorge" 
+age = 20
 
-# name = input("Digite seu nome: ")
-# age = input(f"Digite sua idade, {name}: ")
+name = input("Digite seu nome: ")
+age = input(f"Digite sua idade, {name}: ")
 
-# print("Sou " + name + " e tenho " + str(age) + " anos")
-# print(f'Sou {name} tenho {age} anos e {height} de altura')
+print("Sou " + name + " e tenho " + str(age) + " anos")
+print(f'Sou {name} tenho {age} anos e {height} de altura')
 
-# new_age = float(age)
-# new_height = int(height)
+new_age = float(age)
+new_height = int(height)
 
-# print(new_age)
-# print(type(new_age))
+print(new_age)
+print(type(new_age))
 
-# print(new_height)
-# print(type(new_height))
+print(new_height)
+print(type(new_height))
 
-# Collections 
+# structural types or collections
 
-list = ["string", 2, True, [2, "Três"]]
+list = ["string", 2, True, [2, "três"]]
 #           0      1    2         3
 #          -4     -3   -2        -1
 
-# print(list[0])
-# print(list[-2])
-# print(list[-1][1])
+print(list[0])
+print(list[-2])
+print(list[-1][1])
 
 list[2] = False
 list[-1][0] = "D"
 
-# print(list)
-# print(len(list))
+print(list)
+print(len(list))
 
 tuple = ("string", 2, True, [2, "Três"])
 # tuple = ("cpf", "123.123.123-12", 12312312312)
 
-# print(tuple[0])
-# print(tuple[-2])
-# print(tuple[3][1])
+print(tuple[0])
+print(tuple[-2])
+print(tuple[3][1])
 
-# tuple[2] = False
+tuple[2] = False # error: tuple is immutable
 
-# print(tuple)
-# print(len(tuple))
+print(tuple)
+print(len(tuple))
 
-set = {"Violão", "Guitarra", "Bateria"}
-set.add("Baixo")
-# set.remove("Violão")
+set = {"violão", "guitarra", "bateria"}
+set.add("baixo")
+set.remove("violão")
 set.pop()
-# print(set)
+print(set)
 
 dict = {
   "name":"Emanuel",
   "age":30,
   "is_admin": False,
-  "favorites": ["Matrix", "Jurassic Park", "The Terminator"]
+  "favorites": ["matrix", "jurassic park", "the terminator"]
 }
 
-# print(dict["favorites"][1])
+print(dict["favorites"][1])
 print(dict.get("favorites", "Não foi possível recuperar dado")[1])
