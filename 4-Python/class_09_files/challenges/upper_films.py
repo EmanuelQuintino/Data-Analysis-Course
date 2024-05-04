97a375b8ac7a87a96f8670edb656fa2a0cd43c69
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-05-03 21:39:16 -0300

    update class files

--- 4-Python/class_09_files/challenges/upper_films.py
+++ 4-Python/class_09_files/challenges/upper_films.py
@@ -4,10 +4,11 @@
 
 try:
   with open("4-Python/class_09_file_manipulation/files/films.txt", "r", encoding="utf-8") as films, \
-     open("4-Python/class_09_file_manipulation/files/file.txt", "w", encoding="utf-8") as file:
+       open("4-Python/class_09_file_manipulation/files/file.txt", "w", encoding="utf-8") as file:
 
     for film in films:
       file.write(film.upper())
+
 except FileNotFoundError:
   print("arquivo não encontrado!")
   
