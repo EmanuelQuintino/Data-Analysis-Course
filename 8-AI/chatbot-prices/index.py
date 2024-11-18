add0d9f9103aa9007fa4618ae7327607478f8e63
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-11-18 01:20:28 -0300

    add chat bot prices

---
+++ 8-AI/chatbot-prices/index.py
@@ -0,0 +1,59 @@
+import speech_recognition as sr
+import pyttsx3
+from products_data import products  
+
+def user_speech_recognition():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Escutando...\n")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+        try:
+            text = recognizer.recognize_google(audio, language="pt-BR").lower()
+            return text
+        except sr.UnknownValueError:
+            print(f"\033[4mChatbot\033[0m: Não entendi o que falou, por favor fale novamente!\n")
+        except sr.RequestError as error:
+            print(f"Erro ao solicitar resultados: {error}")
+
+def talk(text):
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 200)
+    engine.setProperty("volume", 1)
+    engine.say(text)
+    engine.runAndWait()
+
+def search_response(message):
+    if any(greeting in message for greeting in ["olá", "oi", "bom dia", "boa tarde", "boa noite"]):
+        return "Olá, tudo bem? Por favor, fale o nome do produto para saber preço ou descrição."
+
+    if "preço" in message:
+        for product in products:
+            if any(tag in message for tag in product['tags']):
+                return f"O preço d{product['genero']} {product['name']} é {product['preço']} reais."
+
+    if any(keyword in message for keyword in ["descrição", "mais sobre", "me conte", "saber mais"]):
+        for product in products:
+            if any(tag in message for tag in product['tags']):
+                return f"A descrição d{product['genero']} {product['name']} é: {product['descricao']}."
+
+    return "Posso te ajudar com preço e descrição. Se quiser saber mais irei chamar um de nossos vendedores para melhor te atender."
+
+
+while True:
+    user_message = user_speech_recognition()
+
+    if user_message:
+        print(f"Cliente: {user_message}")  
+
+        if any(keyword in user_message for keyword in ["sair", "parar", "encerrar", "obrigado"]):
+            print("Zê: Obrigado, volte sempre!")
+            talk("Obrigado, volte sempre!")
+            break
+
+        bot_response = search_response(user_message)
+        print(f"Zê: {bot_response}")  
+        talk(bot_response)
+    else:
+        print("Zê: Não entendi o que falou, por favor fale novamente!")
---
+++ 8-AI/chatbot-prices/products_data.py
@@ -0,0 +1,34 @@
+products = [
+    {
+        "cod": "001",
+        "name": "TV ou Televisão de 50 polegadas",
+        "genero": "a",
+        "preço": 2500,
+        "descricao": "A TV de 50 polegadas oferece a melhor tela do mercado com resolução 4k para uma experiência visual imersiva.",
+        "tags": ["tv", "televisão", "50 polegadas", "smart tv", "televisão grande"]
+    },
+    {
+        "cod": "002",
+        "name": "Geladeira Frost Free 500L",
+        "genero": "a",
+        "preço": 3500,
+        "descricao": "Geladeira de 500 litros, tecnologia frost free para evitar o acúmulo de gelo e otimizar o uso de espaço.",
+        "tags": ["geladeira", "frost free", "500L", "refrigerador"]
+    },
+    {
+        "cod": "003",
+        "name": "Sofá Retrátil 3 Lugares Paris",
+        "genero": "o",
+        "preço": 1800,
+        "descricao": "Sofá retrátil de 3 lugares, perfeito para relaxar e acomodar toda a família com conforto e estilo.",
+        "tags": ["sofá", "retrátil", "3 lugares", "paris"]
+    },
+    {
+        "cod": "004",
+        "name": "Fogão 5 Bocas Inox",
+        "genero": "o",
+        "preço": 1500,
+        "descricao": "Fogão inox de 5 bocas, ideal para quem precisa de mais espaço para cozinhar e agilidade na cozinha.",
+        "tags": ["fogão", "5 bocas", "inox"]
+    }
+]
